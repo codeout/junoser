d2a1b1250b87ae5d7a7178bdf40a5342024d811e
Shintaro Kojima <goodies@codeout.net>
2022-09-20 02:00:54 +0900

    fix: "classifiers" for QoS

--- lib/junoser/ruler.rb
+++ lib/junoser/ruler.rb
@@ -200,12 +200,11 @@ module Junoser
       # Fix .xsd: "ieee-802.3ad" is invalid
       str.gsub! '"ieee-802.3ad"', '"802.3ad"'
 
-      # Fix .xsd: "class-of-service interfaces all unit * classifiers exp foo"
-      str.gsub!(/^(\s*)sc\(\s*\("default"\)\s*\)/) do
-        format(['c(',
-                '  ("default" | arg)',
-                ')'], $1)
-      end
+      # Fix .xsd: "classifiers"
+      str.gsub! '("default")', '("default" | arg)'
+
+      # Fix .xsd: "class-of-service interfaces xxx unit x"
+      str.gsub! '"*"', 'arg'
 
       # Fix .xsd: "from-zone" arg is also required
       str.gsub!(/^(\s*)"policy" \(\s*s\(\s*arg,\s*"to-zone-name" arg,\s*c\(\s*"policy" \(\s*policy_type\s*\)\s*\)/) do
